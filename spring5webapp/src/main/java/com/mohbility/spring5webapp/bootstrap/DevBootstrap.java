721e6252f36c3f42aab86c30372a02bae879c1df
home <home@10.0.0.122>
2018-10-12 02:02:58 -0400

    Assignment completed

--- spring5webapp/src/main/java/com/mohbility/spring5webapp/bootstrap/DevBootstrap.java
+++ spring5webapp/src/main/java/com/mohbility/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,36 +6,45 @@ import org.springframework.stereotype.Component;
 
 import com.mohbility.spring5webapp.model.Author;
 import com.mohbility.spring5webapp.model.Book;
+import com.mohbility.spring5webapp.model.Publisher;
 import com.mohbility.spring5webapp.repositories.AuthorRepository;
 import com.mohbility.spring5webapp.repositories.BookRepository;
+import com.mohbility.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 	
 	
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 
 	private void initData() {
-	Author eric = new Author("Eric", "Evans");
-	Book ddd = new Book("Domain Driven Design","1234", "Harper Collins");
-	eric.getBooks().add(ddd);
-	ddd.getAuthors().add(eric);
-	
-	authorRepository.save(eric);
-	bookRepository.save(ddd);
-	
-	Author rod = new Author("Rod", "Johnson");	
-	Book noEJB = new Book("J2EE Development without EJB","23444", "Worx");
-	rod.getBooks().add(noEJB);
-	
-	authorRepository.save(rod);
-	bookRepository.save(noEJB);
-	
+		
+		Publisher publisher1 = new Publisher("Harper Collins", "971 Grant Street");
+		publisherRepository.save(publisher1);
+		Author eric = new Author("Eric", "Evans"); 
+		Book ddd = new Book("Domain Driven Design", "1234", publisher1);
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+		
+		Publisher publisher2 = new Publisher("worx", "12 James Street");
+		publisherRepository.save(publisher2);
+		Author rod = new Author("Rod", "Johnson");
+		Book noEJB = new Book("J2EE Development without EJB", "23444", publisher2);
+		rod.getBooks().add(noEJB);
+
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+
 	}
 
 	@Override
---
+++ spring5webapp/src/main/java/com/mohbility/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,11 @@
+package com.mohbility.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mohbility.spring5webapp.model.Publisher;
+
+
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
